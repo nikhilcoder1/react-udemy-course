ba062e0432144fc8706f5489742d38fce31db873
dev_nick <developernikhil28@gmail.com>
2025-08-05 18:48:49 +0530

    Updated app.js and app.css

--- 02_counter/src/app.jsx
+++ 02_counter/src/app.jsx
@@ -4,40 +4,36 @@ import viteLogo from '/vite.svg'
 import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+
+  const [counter , setcounter] = useState(0); // counter : is a variable , setcounter : is a function
+
+  // let counter = 10;
+
+  const addValue = () => {
+    // to prevent batching in react using previousCounter    
+    setcounter((prevCounter) => prevCounter + 1);
+    setcounter((prevCounter) => prevCounter + 1);
+    setcounter((prevCounter) => prevCounter + 1);
+    setcounter((prevCounter) => prevCounter + 1);
+  }
+
+  const subValue = () => {
+    setcounter(counter - 1);
+    if(counter <= 0) {
+      setcounter(0);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>React Course {counter} </h1>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={subValue}>Remove Value</button>
+      <p>Footer : {counter}</p>
+    </div>
   )
 }
+
+
+export default App;
