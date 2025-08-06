0b1759b2e08483030cbac41ff755eb4fd79e9408
dev_nick <developernikhil28@gmail.com>
2025-08-06 14:31:00 +0530

    Modified some files

--- 05_bgChanger/src/App.jsx
+++ 05_bgChanger/src/App.jsx
@@ -4,13 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('orange');
+
+  // function changeColor(color){
+  //   setColor(color);
+  // }
 
   return (
-    <>
-      <h1 className='text-blue-600 bg-green-500 p-3 rounded-md'>Background Changer App(Vite + Tailwind)</h1>
-    </>
-  )
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color}}>
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+      
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
+      
+          <button onClick={() => setColor('red')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'red'}}>Red</button>
+          <button onClick={() => setColor('green')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'green'}}>Green</button>
+          <button onClick={() => setColor('blue')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'blue'}}>Blue</button>
+          <button onClick={() => setColor('yellow')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'yellow'}}>Yellow</button>
+          <button onClick={() => setColor('#1a1a1a')}
+          className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor:'#1a1a1a'}}>Grey</button>
+      
+        </div>
+      
+      </div>
+    
+    </div>
+  
+)
 }
 
 export default App
