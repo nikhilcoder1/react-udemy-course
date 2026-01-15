0415ab84e7fb15aae1909cff86032547145ca17e
dev_nick <developernikhil28@gmail.com>
2026-01-16 00:28:44 +0530

    React Router Dom Project Done

--- 08_reactRouter/src/main.jsx
+++ 08_reactRouter/src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+import Github, { githubInfoLoader } from './components/GitHub/GitHub.jsx'
 
 // const router = createBrowserRouter([
 //   {
