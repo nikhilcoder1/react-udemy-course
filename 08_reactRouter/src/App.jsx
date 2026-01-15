ae7c46ea8e59b24e4bea8d96dbf0260e75fca2d6
dev_nick <developernikhil28@gmail.com>
2026-01-15 23:34:41 +0530

    Components Uploaded

--- 08_reactRouter/src/App.jsx
+++ 08_reactRouter/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='text-green-700 p-4 text-center'>React Router Dom</h1>
+    <>
+     
+      
+    </>
   )
 }
 
-export default App
+export default App;
